a914e132c9bfe4cf29af6b3bfc54fd0a1140685a
kapiljhajhria <jhajhria44@gmail.com>
2020-12-07 20:37:07 +0530

    twp ways to link pages
    a tag and Link from gatsby

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const IndexPage = () => {
   return (
     <div>
       <h1>Hello,</h1>
       <h2>I am kapil from delhi,India</h2>
+      <p>
+        Need a developer? <a href="/contact-us">Contact Me</a>
+      </p>
+      <p>
+        <Link to={"/contact-us"}>Contact Us</Link>
+      </p>
     </div>
   )
 }
