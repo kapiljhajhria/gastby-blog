0a6f402ddfd45ee6bc0a2d258ff8555822be840d
kapiljhajhria <jhajhria44@gmail.com>
2020-12-07 20:31:48 +0530

    new pages created. aboutus, blog, contactus

---
+++ src/pages/blog.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>This will be a blog page.</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit ut in
+        nam praesentium nesciunt placeat culpa! Ut sunt recusandae libero, a
+        aliquam esse omnis consequuntur cum consectetur? Temporibus, id vero!
+      </p>
+    </div>
+  )
+}
+
+export default BlogPage
