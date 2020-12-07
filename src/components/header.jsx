0dff992f98777df14023daaa295ff7ce6852bdf3
kapiljhajhria <jhajhria44@gmail.com>
2020-12-07 21:43:19 +0530

    heder modified

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,16 +2,26 @@ import React from "react"
 import { Link } from "gatsby"
 
 const Header = () => {
-    return (
-        <div>
-            <h1>My Website Title</h1>
-            <div>
-                <Link to="/"> Home</Link>
-                <Link to="/about"> About</Link>
-                <Link to="/contact"> Contact</Link>
-                <Link to="/blog"> Blog</Link>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1>My Website Title</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/"> Home</Link>{" "}
+          </li>
+          <li>
+            <Link to="/about"> About</Link>{" "}
+          </li>
+          <li>
+            <Link to="/contact"> Contact</Link>{" "}
+          </li>
+          <li>
+            <Link to="/blog"> Blog</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
 }
-export default Header;
+export default Header
