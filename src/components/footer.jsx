df3d3f38ffc4fa9499daf7022e8616de976401e4
kapil jhajhria <jhajhria44@gmail.com>
2021-02-21 22:28:03 +0530

    adding images to posts and styled blog page

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,8 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+
+import footerStyles from "./footer.module.scss"
+
 const Footer = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -13,7 +16,7 @@ const Footer = () => {
     }
   `)
   return (
-    <footer>
+    <footer className={footerStyles.footer}>
       <p>Created by {data.site.siteMetadata.student}, @ 2020</p>
     </footer>
   )
