b665c3fdeeed6b4b71bb8958a8b3b0ac48dda143
kapil jhajhria <jhajhria44@gmail.com>
2021-02-22 11:38:44 +0530

    modified title

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: "Gatsby Bootcamp",
+    title: "Gatsby Contentful Site",
     author: "Andrew Mead",
     student: "Kapil Jhajhria",
   },
