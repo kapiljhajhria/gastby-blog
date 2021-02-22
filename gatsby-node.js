41da1175f898beeeac1778416e0b93cfe1cf1447
kapil jhajhria <jhajhria44@gmail.com>
2021-02-22 11:11:28 +0530

    rendering blog posts pages from contentful

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,5 @@
 const path = require("path")
 
-module.exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === "MarkdownRemark") {
-    const slug = path.basename(node.fileAbsolutePath, ".md")
-
-    console.log("new node created---------------@@@@@@@@@@@@--------", slug)
-
-    createNodeField({
-      node,
-      name: "slug",
-      value: slug,
-    })
-  }
-}
-
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
@@ -22,24 +7,22 @@ module.exports.createPages = async ({ graphql, actions }) => {
 
   const res = await graphql(`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost {
         edges {
           node {
-            fields {
-              slug
-            }
+            slug
           }
         }
       }
     }
   `)
 
-  res.data.allMarkdownRemark.edges.forEach(edge => {
+  res.data.allContentfulBlogPost.edges.forEach(edge => {
     createPage({
       component: blogTemplate,
-      path: `/blog/${edge.node.fields.slug}`,
+      path: `/blog/${edge.node.slug}`,
       context: {
-        slug: edge.node.fields.slug,
+        slug: edge.node.slug,
       },
     })
   })
